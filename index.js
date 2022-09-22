9df4652df1ab3bdbbae2cf1d98e6e2034c40280a
mugilan143 <mugilanm.19cse@kongu.edu>
2022-09-22 09:45:29 +0530

    new

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 require('dotenv').config();
+const mongoose = require("mongoose");
 const port=process.env.PORT||3000;
 const app= express();
 app.use(express.static("public"));
@@ -10,7 +10,21 @@ app.set('view engine','ejs');
 app.use(bodyparser.urlencoded({
     extended:true
 }));
- 
+const DB ="mongodb+srv://mugilan:19csr111@cluster0.jcb36.mongodb.net/datadb?retryWrites=true&w=majority";
+mongoose.connect(DB 
+ ).then(()=>{
+    console.log("connection successfull");
+}).catch((err)=>console.log(err));
+
+const userSchema =new mongoose.Schema({
+
+    name:String,
+    email:String,
+    password:String
+    
+});
+
+const User = new mongoose.model("user",userSchema);
 
 app.get("/",function(req,res){
     res.render("home");
@@ -23,6 +37,47 @@ app.get("/login",function(req,res){
 })
 app.get("/signup",function(req,res){
     res.render("signup");
+})
+app.post("/register",function(req,res){
+    const newUser = new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    });
+    newUser.save(function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+            res.redirect("error");
+        }
+        else{
+            res.render("login");
+        }
+    })
+})
+app.post("/login",function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email:email},function(err,foundUser){
+        if(err)
+        {
+            console.log(err);
+            res.redirect("error");
+        }
+        else{
+            if(foundUser)
+            {
+                if(foundUser.password === password)
+                {
+                    console.log(foundUser);
+                    res.render("admin");
+                }
+
+            }
+        }
+    })
 })
 app.listen(port,()=> 
 console.log("server run on port at http://localhost:3000"));
